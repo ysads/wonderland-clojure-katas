ac691709380aab8fb096b139c7998986d88b06e3
Ygor Sad <ygor.sad@revelo.com.br>
2019-10-09 20:56:36 -0300

    [deps] add autotest plugin

--- alphabet-cipher/project.clj
+++ alphabet-cipher/project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :main alphabet-cipher.coder)
+  :main alphabet-cipher.coder
+  :plugins [[lein-auto "0.1.3"]])
